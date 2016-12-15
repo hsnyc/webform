af887502c45ee489efff523acf921f53a6a34869
Henry Sanchez <henry@hsnyc.co>
2016-12-14 20:05:34 -0500

    Edited php file for this specific form.

--- public/js/validate.php
+++ public/js/validate.php
@@ -1,25 +1,25 @@
 <?php
-/***********************************************************************************************/
+/*****************************************************************************
 /* If nothing is posted then we exit */
-/***********************************************************************************************/
+/****************************************************************************/
 if (!$_POST) {
 	die("This file cannot be accessed directly!");
 }
 
 
 
-/***********************************************************************************************/
+/*****************************************************************************
 /* Define regular expression patterns */
-/***********************************************************************************************/
+/****************************************************************************/
 $expEmail = 			'/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/';
 $expLettersOnly = 		'/^[a-zA-Z ]+$/';
 $expLettersNumbers = 	'/^[a-zA-Z0-9]*$/';
 
 
 
-/***********************************************************************************************/
+/**************************************************************************************
 /* Define the function for checking the field length */
-/***********************************************************************************************/
+/*************************************************************************************/
 function validateLength($fieldValue, $minLength) {
 	return (strlen(trim($fieldValue)) > $minLength);
 }
@@ -30,66 +30,38 @@ function validateLength($fieldValue, $minLength) {
 /* Get the posted field values and validate each field */
 /***********************************************************************************************/
 $name 				= $_POST["name"];
-$username 			= $_POST["username"];	
-$gender 			= $_POST["gender"];	
-$email 				= $_POST["email"];	
-$password 			= $_POST["password"];	
-$confirmPassword	= $_POST["confirm-password"];	
-$subscribe 			= $_POST["subscribe"];
+$email 				= $_POST["email"];
+$message			= $_POST["message"];
 
 $errorExists 		= false;
 $errors 			= "Errors: <ul>";
 
 // Name
-if (!validateLength($name, 2)) {
+if ($name == "" || $name == null) {
 	$errorExists = true;
-	$errors .= "<li>The name is too short!</li>";
+	$errors .= "<li>Please enter a name!</li>";
 }
-if (preg_match($expLettersOnly, $name) !== 1) {
+
+elseif (preg_match($expLettersOnly, $name) !== 1) {
 	$errorExists = true;
 	$errors .= "<li>The name can only contain letters and spaces!</li>";
 }
 
-// Username
-if (!validateLength($username, 2)) {
-	$errorExists = true;
-	$errors .= "<li>The username is too short!</li>";
-}
-if (preg_match($expLettersNumbers, $username) !== 1) {
-	$errorExists = true;
-	$errors .= "<li>The username can only contain alphanumeric characters!</li>";
-}
-
-// Gender
-if ($gender === "1") {
-	$gender = "Male";
-} else if ($gender === "2") {
-	$gender = "Female";
-} else {
-	$errorExists = true;
-	$errors .= "<li>Please select a gender!</li>";
-}
-
 // Email
-if (preg_match($expEmail, $email) !== 1) {
+if ($email == "" || $email == null) {
+	$errorExists = true;
+	$errors .= "<li>Please enter an email!</li>";
+}
+
+elseif (preg_match($expEmail, $email) !== 1) {
 	$errorExists = true;
 	$errors .= "<li>The email address format is invalid!</li>";
 }
 
-// Password
-if (!validateLength($password, 5)) {
+// message
+if ($message == "" || $message == null) {
 	$errorExists = true;
-	$errors .= "<li>The password is too short!</li>";
-}
-if (preg_match($expLettersNumbers, $password) !== 1) {
-	$errorExists = true;
-	$errors .= "<li>The password can only contain alphanumeric characters!</li>";
-}
-
-// Confirm Password
-if ($confirmPassword !== $password) {
-	$errorExists = true;
-	$errors .= "<li>The passwords don't match!</li>";
+	$errors .= "<li>Please enter a message!</li>";
 }
 
 // If no errors, echo the results
@@ -98,13 +70,14 @@ if (!$errorExists) {
 		. "<p>Details:</p>"
 		. "<ul>"
 		. "<li>Name: $name</li>"
-		. "<li>Usernname: $username</li>"
-		. "<li>Gender: $gender</li>"
 		. "<li>Email: $email</li>"
-		. "<li>Subscribe to newsletter: $subscribe</li>"
+		. "<li>Message: $message</li>"
 		. "</ul>";
+
+		//send form here
+
 } else {
 	echo "<h3>Error! Please address the following issues:</h3>"
 		. $errors;
-}	
+}
 ?>
